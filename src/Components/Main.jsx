f958c733c8863d30eba0401cb5d3b0c6de06a0e3
Alessandro-Frisone <alefri2004@gmail.com>
2025-02-12 19:17:40 +0100

    feat: add flags to languages

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,20 +1,45 @@
 import { useDataContext } from "../contexts/DataContext";
 
+const languageFlags = {
+    en: "https://flagcdn.com/w40/gb.png",
+    it: "https://flagcdn.com/w40/it.png",
+    fr: "https://flagcdn.com/w40/fr.png",
+    es: "https://flagcdn.com/w40/es.png",
+    de: "https://flagcdn.com/w40/de.png",
+    ja: "https://flagcdn.com/w40/jp.png",
+    zh: "https://flagcdn.com/w40/cn.png",
+  };
+
 export default function Main() {
   const { movies } = useDataContext();
+
+  const getFlag = (language) => {
+    const flagUrl = languageFlags[language] || "https://flagcdn.com/w40/un.png";
+    return (
+      <img 
+        src={flagUrl} 
+        alt={language} 
+        width="25" 
+        height="15" 
+      />
+    );
+  };
+
   return (
     <main>
       <h2>Lista film</h2>
+      
       <ul>
         {movies.map((movie) => (
           <li key={movie.id}>
             <p>Titolo: {movie.title}</p>
             <p>Titolo orginale: {movie.original_title}</p>
-            <p>Lingua: {movie.original_language}</p>
+            <p>Lingua: {getFlag(movie.original_language)} ({movie.original_language})</p>
             <p>Voto: {movie.vote_average}</p>
           </li>
         ))}
       </ul>
     </main>
+    
   );
 }
