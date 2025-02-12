d60e5fb0736bd63a7853caa308610f197d496b7e
Alessandro-Frisone <alefri2004@gmail.com>
2025-02-12 14:30:02 +0100

    INIT PROJECT

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// ----- CHIAVE API -----//
+// 74c8ccfcf72cb82e3a700d9396ba6782 //
+// ----- CHIAVE API -----//
+
+//-------- IMPORT COMPONENTS --------//
+import Header from "./Components/Header";
+import Main from "./Components/Main";
+
+export default function App() {
+  
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
+}
---
+++ src/Components/SearchBar.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+export default function SearchBar() {
+  const [search, setSearch] = useState("");
+
+  return (
+    <form>
+      <input
+        name="search"
+        type="search"
+        placeholder="Cerca..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <button type="submit">Cerca</button>
+    </form>
+  );
+}
